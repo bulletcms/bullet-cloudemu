eb87ae829834a3119beecb3afd3cce513ef02980
Kevin Wang <wangkevin448@gmail.com>
2016-04-24 12:55:16 -0700

    using gcd script temporarily

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 if ! [ -e "$PWD/grpc/gcd.sh" ]; then
-  wget https://storage.googleapis.com/gcd/tools/gcd-grpc-1.0.0.zip && 7z e gcd-grpc-1.0.0.zip -o "$PWD/grpc" && rm gcd-grpc-1.0.0.zip
+  wget https://storage.googleapis.com/gcd/tools/gcd-grpc-1.0.0.zip && 7z e gcd-grpc-1.0.0.zip -o"grpc" && rm gcd-grpc-1.0.0.zip
 fi
